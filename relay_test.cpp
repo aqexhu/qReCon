33c1df3716278e02cfbb65da8ee0be4c0a4cdf66
John <balazs.kiss@aqex.hu>
2022-04-12 12:32:32 +0200

    C relay test.

--- relay_test.cpp
+++ relay_test.cpp
@@ -1,4 +1,7 @@
 #include <gpiod.h>
+#include <stdio.h>
+#include <errno.h>
+#include <time.h>
 
 #define RELAY_NUM 4
 #define GPIOD_RELAY1 26
@@ -6,26 +9,47 @@
 #define GPIOD_RELAY3 13
 #define GPIOD_RELAY4 6
 
-int relay_pins[4] = { GPIOD_RELAY1, GPIOD_RELAY2, GPIOD_RELAY3, GPIOD_RELAY4 }
+int relay_pins[4] = {GPIOD_RELAY1, GPIOD_RELAY2, GPIOD_RELAY3, GPIOD_RELAY4};
 
 struct gpiod_chip *chip;
 struct gpiod_line *relay[RELAY_NUM];
 
-void relay_test() {
+void delay(int del_ms)
+{
+    clock_t start = clock();
+    while (clock() < start + del_ms)
+        ;
 }
 
-void init_gpios() {
+void relay_test()
+{
+    for (short i = 0; i < RELAY_NUM; i++)
+    {
+        gpiod_line_set_value(relay[i], 1);
+        delay(2000);
+        gpiod_line_set_value(relay[i], 0);
+    }
+}
+
+void init_gpios()
+{
     const char *chipname = "gpiochip0";
     chip = gpiod_chip_open_by_name(chipname);
     if (!chip)
     {
-        syslog(LOG_ERR, "Open chip failed\n");
+        perror("Open chip failed\n");
+        printf("Buuu");
         exit(0);
     }
-for (short i=0; i<RELAY_NUM; i++) {
-relay[i] = gpiod_chip_get_line(chip, relay_pins[i]);
-}
+    for (short i = 0; i < RELAY_NUM; i++)
+    {
+        relay[i] = gpiod_chip_get_line(chip, relay_pins[i]);
+    }
 }
 
-void main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
+    init_gpios();
+    relay_test();
+    return 0;
 }
